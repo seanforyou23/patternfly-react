2fdeae4c69f6479673dd81dac58de12c01f6a83f
nicolethoen <nthoen@redhat.com>
2020-05-19 13:58:44 -0400

    fix linting issues

--- packages/react-integration/demo-app-ts/src/components/demos/TableDemo/TableEditableDemo.tsx
+++ packages/react-integration/demo-app-ts/src/components/demos/TableDemo/TableEditableDemo.tsx
@@ -161,7 +161,7 @@ export class TableEditableDemo extends React.Component<TableProps, TableState> {
                     <SelectOption key={index} value={option.value} id={'uniqueIdRow1Cell5Option' + index} />
                   ))}
                   onToggle={isOpen => {
-                    this.onToggle(isOpen, rowIndex)
+                    this.onToggle(isOpen, rowIndex);
                   }}
                   selections={this.state.selected[rowIndex]}
                 />
@@ -258,7 +258,7 @@ export class TableEditableDemo extends React.Component<TableProps, TableState> {
                     <SelectOption key={index} value={option.value} id={'uniqueIdRow2Cell5Option' + index} />
                   ))}
                   onToggle={isOpen => {
-                    this.onToggle(isOpen, rowIndex)
+                    this.onToggle(isOpen, rowIndex);
                   }}
                   selections={this.state.selected[rowIndex]}
                 />
